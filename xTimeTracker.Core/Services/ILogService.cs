c66f35b1bd168fb136ea0d6155b116e57a990a6c
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-10-28 14:29:43 +0300

    Added service interfaces: ILogService, IProjectService, ITaskService

---
+++ xTimeTracker.Core/Services/ILogService.cs
@@ -0,0 +1,12 @@
+﻿namespace xTimeTracker.Core.Services
+{
+    interface ILogService
+    {
+        Task<bool> CreateLog(Log log);
+        Task<IEnumerable<Log>> GetLogs();
+        Task<IEnumerable<Log>> GetLogsByDateInterval(DateTime startDate, DateTime endDate);
+        Task<IEnumerable<Log>> GetLogsByTask(int taskId);
+        Task<bool> UpdateLog(Log log);
+        Task<bool> DeleteLog(int logId);
+    }
+}
---
+++ xTimeTracker.Core/Services/ITaskService.cs
@@ -0,0 +1,10 @@
+﻿namespace xTimeTracker.Core.Services
+{
+    interface ITaskService
+    {
+        Task<bool> CreateTask(Task task);
+        Task<IEnumerable<Task>> GetTasksByProject(int projectId);
+        Task<bool> UpdateTask(Task task);
+        Task<bool> DeleteProject(int taskId);
+    }
+}
