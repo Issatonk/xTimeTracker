4475d4abc91eea7063c4c3f72985dfb11e4b8eb6
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-10-28 16:16:31 +0300

    Added repository interfaces: ILogRepository, IProjectRepository, ITaskRepository

---
+++ xTimeTracker.Core/Repositories/ILogRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace xTimeTracker.Core.Repositories
+{
+    interface ILogRepository
+    {
+        Task<bool> CreateLog(Log log);
+        Task<IEnumerable<Log>> GetLogs();
+        Task<IEnumerable<Log>> GetLogsByDateInterval(DateTime startDate, DateTime endDate);
+        Task<IEnumerable<Log>> GetLogsByTask(int taskId);
+        Task<bool> UpdateLog(Log log);
+        Task<bool> DeleteLog(int logId);
+    }
+}
---
+++ xTimeTracker.Core/Repositories/ITaskRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace xTimeTracker.Core.Repositories
+{
+    interface ITaskRepository
+    {
+        Task<bool> CreateTask(Task task);
+        Task<IEnumerable<Task>> GetTasksByProject(int projectId);
+        Task<bool> UpdateTask(Task task);
+        Task<bool> DeleteProject(int taskId);
+    }
+}
