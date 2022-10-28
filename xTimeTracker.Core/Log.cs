6c33c7eece1b69b497885c21b1d842217d3fb21d
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-10-28 14:02:51 +0300

    Added entities: Project, Task, Log

---
+++ xTimeTracker.Core/Log.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace xTimeTracker.Core
+{
+    public class Log
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public TimeSpan TimeSpent {get; set; }
+    }
+}
---
+++ xTimeTracker.Core/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace xTimeTracker.Core
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public TimeSpan Plan { get; set; }
+
+        public TimeSpan TimeSpent { get; set; }
+
+        public int Percent => (int)(TimeSpent / Plan * 100);
+    }
+}
