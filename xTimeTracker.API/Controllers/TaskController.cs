d9fef7b8aaa161c6b4abba2bcea206545d7f876b
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-11-26 00:47:03 +0300

    fixed status codes for TaskController

--- xTimeTracker.API/Controllers/TaskController.cs
+++ xTimeTracker.API/Controllers/TaskController.cs
@@ -24,56 +24,81 @@ namespace xTimeTracker.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(TaskCreateRequest taskRequest)
         {
-            var task = _mapper.Map<TaskCreateRequest, Core.Task>(taskRequest);
-            var result = await _taskService.Create(task);
-            _logger.LogInformation("post\n\tDateTime: {0}\n\tRequest: {1}\n\tResponse: {2} ", DateTime.Now, JsonSerializer.Serialize(taskRequest), result);
-            if (!result)
+            try
             {
-                return BadRequest();
+                var task = _mapper.Map<TaskCreateRequest, Core.Task>(taskRequest);
+                var result = await _taskService.Create(task);
+                if (!result)
+                {
+                    return StatusCode(417, "ExpectationFailed");
+                }
+                _logger.LogInformation("post\n\tDateTime: {0}\n\tRequest: {1}\n\tResponse: {2} ", DateTime.Now, JsonSerializer.Serialize(taskRequest), result);
+                return Ok();
             }
-            return Ok();
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetByProject(int projectId)
         {
-            var result = await _taskService.GetTasksByProject(projectId);
-            _logger.LogInformation("get\n\tDateTime: {0}", DateTime.Now);
-            if (result == null)
+            try
             {
-                return BadRequest();
+                var result = await _taskService.GetTasksByProject(projectId);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                _logger.LogInformation("get\n\tDateTime: {0}", DateTime.Now);
+                return Ok(result);
             }
-            return Ok(result);
+            catch(Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut]
         public async Task<IActionResult> Update(TaskUpdateRequest taskRequest)
         {
-            var task = _mapper.Map<TaskUpdateRequest, Core.Task>(taskRequest);
-            var result = await _taskService.Update(task);
-
-            _logger.LogInformation("put\n\tDateTime: {0}\n\tRequest: {1}\n\tResponse: {2} ", DateTime.Now, JsonSerializer.Serialize(taskRequest), result);
-
-
-            if (!result)
+            try
             {
-                return BadRequest();
+                var task = _mapper.Map<TaskUpdateRequest, Core.Task>(taskRequest);
+                var result = await _taskService.Update(task);            
+
+                if (!result)
+                {
+                    return StatusCode(417, "ExpectationFailed");
+                }
+                _logger.LogInformation("put\n\tDateTime: {0}\n\tRequest: {1}\n\tResponse: {2} ", DateTime.Now, JsonSerializer.Serialize(taskRequest), result);
+                return Ok();
             }
-            return Ok();
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpDelete]
         public async Task<IActionResult> Delete(int taskId)
         {
-            var result = await _taskService.Delete(taskId);
-
-            _logger.LogInformation("delete\n\tDateTime: {0}\n\tRequest: projectId = {1}\n\tResponse: {2} ", DateTime.Now, taskId, result);
-
-            if (!result)
+            try
             {
-                return BadRequest();
+                var result = await _taskService.Delete(taskId);
+
+                if (!result)
+                {
+                    return StatusCode(417, "ExpectationFailed");
+                }
+                _logger.LogInformation("delete\n\tDateTime: {0}\n\tRequest: projectId = {1}\n\tResponse: {2} ", DateTime.Now, taskId, result);
+                return Ok();
             }
-            return Ok();
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
